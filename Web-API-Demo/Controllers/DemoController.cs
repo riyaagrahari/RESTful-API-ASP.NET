2f805217a45632f9f2463e3df0ec0773f44b4610
riyaagrahari <riyaagrahari121@gmail.com>
2019-09-02 14:08:12 +0530

    Initial commit

---
+++ Web-API-Demo/Controllers/DemoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+using DemoDataAccess;
+
+namespace Web_API_Demo.Controllers
+{
+    public class DemoController : ApiController
+    {
+        public IEnumerable<Demo> Get()
+        {
+            using(DemoDBEntities entities = new DemoDBEntities())
+            {
+                return entities.Demoes.ToList();
+
+            }
+        }
+
+        public Demo Get(int id)
+        {
+            using (DemoDBEntities entities = new DemoDBEntities())
+            {
+                return entities.Demoes.FirstOrDefault(e => e.ID == id);
+
+            }
+        }
+    }
+}
